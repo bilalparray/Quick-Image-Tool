9674fdfe6146af73d2ffc1f6e94a62c9dc4fa902
Bilal Ahmad <dev.renosoftwares@gmail.com>
2025-01-23 11:15:42 +0530

    icons update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ConverterComponent } from './converter/converter.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'converter', pathMatch: 'full' },
   { path: 'converter', component: ConverterComponent },
+  { path: 'home', component: HomeComponent },
   { path: '**', redirectTo: 'home' },
 ];
